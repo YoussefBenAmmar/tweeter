7baa5f89f20691aec0a8d5f0cc3b210dec2ab1f1
YoussefBenAmmar <benammar.y@gmail.com>
2023-02-01 14:59:46 -0500

    fetch tweets with Ajax and upadte page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $( "#submit" ).submit(function( event ) {
-//   alert( "Handler for .submit() called." );
-//   event.preventDefault();
-// });
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+   const data = [ ]
 
 
 
 
 const createTweetElement = function (data) {
+
+
 const tweetHTML =
   `<article class="tweet">
   <header>
@@ -50,7 +25,7 @@ const tweetHTML =
   </header>
   <div>"${data.content.text}"</div>
   <footer> 
-    <div>"${data.created_at}"</div>
+    <div>"${timeago.format(data.created_at, 'en_US')}"</div>
     <div>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
@@ -58,59 +33,51 @@ const tweetHTML =
   </footer>
 </article>`
 
+
 $(`#tweet-container`).append(tweetHTML)
+
 };
+
 
 
 const renderTweets = function(data) {
 
+
   $('#tweets-container').empty();
   for (let tweet of data) {
     $('#tweets-container').prepend(createTweetElement(tweet));
+    
   }
+
+  $("form").on("submit", (e) => {
+    console.log("tweet sent");
+    e.preventDefault();
+    console.log( $( "form" ).serialize());
+
+    $.ajax( 'tweets', {
+      method: 'POST',
+      data:$("form").serialize()
+    })
+  });
+
+
+
 };
 
-renderTweets(data);
+const loadTweets = function(data){
+
+  $.get('/tweets',  function(data) {
+    $('#tweets-container').empty();
+    renderTweets(data);
+  })
+
+}
+
+loadTweets(data);
 
 });
 
 
-// const renderTweets = function(data) {
-
-// $(() => {
-//   createTweetElement(data);
-//   $('#tweet-container').on('submit', (evt) => {
-//     evt.preventDefault();
-//     const str = $('#search-input').val();
-//     getMovies(str);
-//   });
-// })
-
-// }
 
 
 
-
-
-// const submitTweet = function (e) {
-//   $("#submit").on("submit", (e) => {
-//     alert("tweet sent");
-//     e.preventDefault();
-//   });
-
-//   $.ajax( 'tweets', {
-//     method: 'POST',
-//     body: $(this).serialize()
-//   })
-// };
-
-
-
-// const loadTweets = function(data){
-//   $.get('/tweets',  (data) => {
-//     console.log("Loading your tweet");
-//     console(tweet)
-//   })
-
-//   timeago.format('created_at')
-// }
